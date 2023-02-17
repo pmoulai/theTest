661f08583dd4ff241730a4609c25bf7ff0184d9d
pmoulai <philippe.moulai@ac-grenoble.fr>
2023-02-17 19:37:52 +0100

    Modif index

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/astier', function(req, res, next) {
+  res.render('astier', { nom: 'Astier', type:'Bts' });
+});
+
 module.exports = router;
